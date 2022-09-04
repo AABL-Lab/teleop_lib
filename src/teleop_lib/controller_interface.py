53a7b7c4d59318fb19e67c614850bdf31cafaee0
Isaac Sheidlower <qigloos@gmail.com>
2022-09-04 13:45:31 -0400

    tying controller_interface to input_profile

---
+++ src/teleop_lib/controller_interface.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# Author: Isaac Sheidlower
+
+"""
+    High llevel class to interface with the controller. 
+    This class communicates input_profile.py to get 
+    mappings of controller input to cartesian commands.
+
+"""
+import rospy
+import numpy as np
+from input_profile import InputProfile
+
+class ControllerInterface:
+    def __init__(self, profile="default", estop_index=0):
+        rospy.init_node('controller_interface', anonymous=True)
+        self.profile = profile
+        self.input_profile = InputProfile(name=profile)
+        self.rate = rospy.Rate(1000) # 10hz
+        self.estop_index = estop_index
+
+    def start(self):
+        pass
+    
+    def stop(self):
+        pass
+
+    def run(self):
+        pass
+
+    
+
+if __name__ == '__main__':
+    try:
+        controller_interface = ControllerInterface()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
--- src/teleop_lib/input_profile.py
+++ src/teleop_lib/input_profile.py
@@ -1,9 +1,57 @@
-#!/usr/bin/env python
-# Author: Isaac Sheid
+#!/usr/bin/env python3
+# Author: Isaac Sheidlower
+"""
+    File the listens to controller joystick input
+    and publishes a twist/cartesian command based on
+    an input profile. An "input profile" is a specification
+    of how the joystick input should be mapped to the
+    cartesian command. 
+
+    #TODO: Add a way to switch between input profiles via
+    dynamic reconfigure.
 """
 
-"""
 import rospy
 import numpy as np
 from sensor_msgs.msg import Joy
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Int32MultiArray
 
+class InputProfile:
+    def __init__(self, name, linear_scale, angular_scale, deadzone):
+        self.name = name
+        self.linear_scale = linear_scale
+        self.angular_scale = angular_scale
+        self.deadzone = deadzone
+        self.rate = 1000
+        self.joy_sub = rospy.Subscriber("joy", Joy, self.joy_callback)
+        self.twist_pub = rospy.Publisher("joy_commanf", Twist, queue_size=10)
+        # Adding higher level button publisher
+        self.button_pub = rospy.Publisher("joy_button", Int32MultiArray, queue_size=10)
+
+    def joy_callback(self, data):
+        # Get the joystick input
+        joy_x = data.axes[0]
+        joy_y = data.axes[1]
+        joy_z = data.axes[2]
+
+        # Apply the deadzone
+        if abs(joy_x) < self.deadzone:
+            joy_x = 0
+        if abs(joy_y) < self.deadzone:
+            joy_y = 0
+        if abs(joy_z) < self.deadzone:
+            joy_z = 0
+
+        # Scale the joystick input
+        self.twist_msg.linear.x = joy_y * self.linear_scale
+        self.twist_msg.linear.y = joy_x * self.linear_scale
+        self.twist_msg.angular.z = joy_z * self.angular_scale
+
+        # Publish the twist message
+        self.twist_pub.publish(self.twist_msg)
+        self.button_pub.publish(data.buttons)
+
+    def run(self):
+        while not rospy.is_shutdown():
+            self.rate.sleep()
