95d718e5383957a2dd653f18b81e5533236ef86e
Reuben Aronson <reuben.aronson@tufts.edu>
2024-02-06 16:26:50 -0500

    use armpy full interface

--- src/teleop_lib/plugins/gen3_robot.py
+++ src/teleop_lib/plugins/gen3_robot.py
@@ -1,16 +1,17 @@
-import armpy.kortex_arm
-
-
+import armpy
 from teleop_lib.msg import RobotCommand
 
 class Gen3Plugin:
     def __init__(self, arm_name):
-        self._robot = armpy.kortex_arm.Arm(arm_name)
-
+        self._robot = armpy.initialize("gen3")
     def do_command(self, cmd):
         if cmd.command == RobotCommand.STOP_COMMAND:
             self._robot.stop()
             # TODO: stop gripper
+        elif cmd.command == RobotCommand.OPEN_GRIPPER:
+            self._robot.open_gripper(block=False)
+        elif cmd.command == RobotCommand.CLOSE_GRIPPER:
+            self._robot.close_gripper(block=False)
         else:
             cmd = [
                 cmd.twist.linear.x, cmd.twist.linear.y, cmd.twist.linear.z,
